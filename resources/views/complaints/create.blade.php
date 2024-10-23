cd49da1eae42114c3d51409b6c8f087f5b0dc369
Jay Bryan Canas <jbryan.canas@gmail.com>
2024-10-24 00:10:14 +0800

    updates

--- resources/views/complaints/create.blade.php
+++ resources/views/complaints/create.blade.php
@@ -92,11 +92,6 @@
                                 <input type="datetime-local" class="form-control" id="incident_date" name="incident_date" value="{{ old('incident_date') }}" required>
                             </div>
                         </div>
-                    </div>
-                </div>
-
-                <div class="card mb-4">
-                    <div class="card-body">
                         <h5 class="card-title">Employee Information</h5>
                         <div class="mb-3">
                             <label for="officer_name" class="form-label">Employee Name</label>
